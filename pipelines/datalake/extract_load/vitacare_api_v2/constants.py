e12dae915d5cc53ae9775ceffbd8e6860395cef4
Daniel Lira <danielllira@icloud.com>
2026-05-25 19:22:31 -0300

    chore: lint

--- pipelines/datalake/extract_load/vitacare_api_v2/constants.py
+++ pipelines/datalake/extract_load/vitacare_api_v2/constants.py
@@ -3,23 +3,24 @@
 """
 Constants for Vitacare.
 """
+
 from enum import Enum
 
 
 class constants(Enum):
-    """
-    Constant values for the dump vitacare flows
-    """
+  """
+  Constant values for the dump vitacare flows
+  """
 
-    INFISICAL_PATH = "/prontuario-vitacare"
-    INFISICAL_VITACARE_USERNAME = "USERNAME"
-    INFISICAL_VITACARE_PASSWORD = "PASSWORD"
+  INFISICAL_PATH = "/prontuario-vitacare"
+  INFISICAL_VITACARE_USERNAME = "USERNAME"
+  INFISICAL_VITACARE_PASSWORD = "PASSWORD"
 
-    ENDPOINT = {
-        "posicao": "/reports/pharmacy/stocks",
-        "movimento": "/reports/pharmacy/movements",
-        "vacina": "/reports/vacinas/listagemvacina",
-        "condicao": "/reports/attendances/attendanceparamcid",
-    }
+  ENDPOINT = {
+    "posicao": "/reports/pharmacy/stocks",
+    "movimento": "/reports/pharmacy/movements",
+    "vacina": "/reports/vacinas/listagemvacina",
+    "condicao": "/reports/attendances/attendanceparamcid",
+  }
 
-    DATASET_ID = "brutos_prontuario_vitacare"
+  DATASET_ID = "brutos_prontuario_vitacare"
--- pipelines/datalake/extract_load/vitacare_api_v2/flows.py
+++ pipelines/datalake/extract_load/vitacare_api_v2/flows.py
@@ -6,13 +6,13 @@ from prefect.storage import GCS
 
 from pipelines.constants import constants
 from pipelines.datalake.extract_load.vitacare_api_v2.constants import (
-    constants as flow_constants,
+  constants as flow_constants,
 )
 from pipelines.datalake.extract_load.vitacare_api_v2.schedules import schedules
 from pipelines.datalake.extract_load.vitacare_api_v2.tasks import (
-    extract_data,
-    generate_endpoint_params,
-    send_email_notification,
+  extract_data,
+  generate_endpoint_params,
+  send_email_notification,
 )
 from pipelines.utils.basics import get_property_from_dict
 from pipelines.utils.flow import Flow
@@ -21,85 +21,72 @@ from pipelines.utils.tasks import rename_current_flow_run, upload_df_to_datalake
 from pipelines.utils.time import from_relative_date
 
 with Flow(
-    name="DataLake - Extração e Carga de Dados - VitaCare API v2",
-    state_handlers=[handle_flow_state_change],
-    owners=[constants.PEDRO_ID.value],
+  name="DataLake - Extração e Carga de Dados - VitaCare API v2",
+  state_handlers=[handle_flow_state_change],
+  owners=[constants.PEDRO_ID.value],
 ) as sms_vitacare_api_v2:
-    #####################################
-    # Parameters
-    #####################################
+  #####################################
+  # Parameters
+  #####################################
 
-    # Flow
-    ENVIRONMENT = Parameter("environment", default="dev")
-    RENAME_FLOW = Parameter("rename_flow", default=False)
+  # Flow
+  ENVIRONMENT = Parameter("environment", default="dev")
+  RENAME_FLOW = Parameter("rename_flow", default=False)
 
-    # Vitacare API
-    RELATIVE_TARGET_DATE = Parameter("target_date", default="D-1")
-    ENDPOINT = Parameter("endpoint", required=True)
+  # Vitacare API
+  RELATIVE_TARGET_DATE = Parameter("target_date", default="D-1")
+  ENDPOINT = Parameter("endpoint", required=True)
 
-    # GCP
-    DATASET_ID = Parameter("dataset_id", default=flow_constants.DATASET_ID.value)
-    TABLE_ID_PREFIX = Parameter("table_id_prefix", default=None)
+  # GCP
+  DATASET_ID = Parameter("dataset_id", default=flow_constants.DATASET_ID.value)
+  TABLE_ID_PREFIX = Parameter("table_id_prefix", default=None)
 
-    #####################################
-    # Tasks
-    #####################################
-    target_date = from_relative_date(relative_date=RELATIVE_TARGET_DATE)
+  #####################################
+  # Tasks
+  #####################################
+  target_date = from_relative_date(relative_date=RELATIVE_TARGET_DATE)
 
-    with case(RENAME_FLOW, True):
-        rename_current_flow_run(
-            environment=ENVIRONMENT,
-            target_date=target_date,
-            endpoint=ENDPOINT,
-        )
-
-    endpoint_params, table_names = generate_endpoint_params(
-        target_date=target_date,
-        environment=ENVIRONMENT,
-        table_id_prefix=TABLE_ID_PREFIX,
+  with case(RENAME_FLOW, True):
+    rename_current_flow_run(
+      environment=ENVIRONMENT, target_date=target_date, endpoint=ENDPOINT
     )
 
-    extraction_results = extract_data.map(
-        endpoint_params=endpoint_params,
-        endpoint_name=unmapped(ENDPOINT),
-        environment=unmapped(ENVIRONMENT),
-    )
+  endpoint_params, table_names = generate_endpoint_params(
+    target_date=target_date, environment=ENVIRONMENT, table_id_prefix=TABLE_ID_PREFIX
+  )
 
-    extracted_data = get_property_from_dict.map(
-        dict=extraction_results,
-        key=unmapped("data"),
-    )
-    logs = get_property_from_dict.map(
-        dict=extraction_results,
-        key=unmapped("logs"),
-    )
+  extraction_results = extract_data.map(
+    endpoint_params=endpoint_params,
+    endpoint_name=unmapped(ENDPOINT),
+    environment=unmapped(ENVIRONMENT),
+  )
 
-    upload_df_to_datalake.map(
-        df=extracted_data,
-        dataset_id=unmapped(DATASET_ID),
-        table_id=table_names,
-        partition_column=unmapped("_loaded_at"),
-        source_format=unmapped("parquet"),
-        if_exists=unmapped("append"),
-        dump_mode=unmapped("append"),
-    )
+  extracted_data = get_property_from_dict.map(
+    dict=extraction_results, key=unmapped("data")
+  )
+  logs = get_property_from_dict.map(dict=extraction_results, key=unmapped("logs"))
 
-    send_email_notification(
-        logs=logs,
-        endpoint=ENDPOINT,
-        environment=ENVIRONMENT,
-        target_date=target_date,
-    )
+  upload_df_to_datalake.map(
+    df=extracted_data,
+    dataset_id=unmapped(DATASET_ID),
+    table_id=table_names,
+    partition_column=unmapped("_loaded_at"),
+    source_format=unmapped("parquet"),
+    if_exists=unmapped("append"),
+    dump_mode=unmapped("append"),
+  )
+
+  send_email_notification(
+    logs=logs, endpoint=ENDPOINT, environment=ENVIRONMENT, target_date=target_date
+  )
 
 
 sms_vitacare_api_v2.schedule = schedules
 sms_vitacare_api_v2.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
 sms_vitacare_api_v2.executor = LocalDaskExecutor(num_workers=5)
 sms_vitacare_api_v2.run_config = KubernetesRun(
-    image=constants.DOCKER_IMAGE.value,
-    labels=[
-        constants.RJ_SMS_AGENT_LABEL.value,
-    ],
-    memory_limit="2Gi",
-    memory_request="2Gi",
+  image=constants.DOCKER_IMAGE.value,
+  labels=[constants.RJ_SMS_AGENT_LABEL.value],
+  memory_limit="2Gi",
+  memory_request="2Gi",
 )
