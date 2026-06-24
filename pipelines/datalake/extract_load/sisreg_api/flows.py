f1d1d2e705d5c1befc0ffe2c92a0c39c83e58862
Matheus Avellar <matheus.avellar@prefeitura.rio>
2026-06-24 10:11:30 -0300

    teste com memory=small

--- pipelines/datalake/extract_load/sisreg_api/flows.py
+++ pipelines/datalake/extract_load/sisreg_api/flows.py
@@ -112,4 +112,4 @@ def extract_sisreg_api(
   # TODO: validação de quais uploads foram bem sucedidos, quais não
 
 
-_flows = [flow_config(flow=extract_sisreg_api, schedules=schedules, memory="large")]
+_flows = [flow_config(flow=extract_sisreg_api, schedules=schedules, memory="small")]
