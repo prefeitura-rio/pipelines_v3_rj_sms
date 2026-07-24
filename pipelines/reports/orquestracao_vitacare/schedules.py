bbf2136512839dc28019ec289adf091bdcdc93fd
Daniel Lira <danielllira@icloud.com>
2026-07-24 16:00:42 -0300

    chore: use dev environment in schedule for testing

--- pipelines/reports/orquestracao_vitacare/schedules.py
+++ pipelines/reports/orquestracao_vitacare/schedules.py
@@ -3,7 +3,8 @@ from pipelines.utils.schedules import create_schedule
 
 schedules = [
   create_schedule(
-    parameters={"environment": "prod"},
+    # TODO: Alterar para "prod" após aprovação dos testes
+    parameters={"environment": "dev"},
     interval="monthly",
     config={"day": 7, "hour": 16, "minute": 0},
   )
