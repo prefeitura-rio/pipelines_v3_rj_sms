494cdeda57e228ae59899f51d840bfa72e705887
Daniel Lira <danielllira@icloud.com>
2026-07-28 14:47:30 -0300

    chore: add new schedule to test flow run (#89)

--- pipelines/reports/orquestracao_vitacare/schedules.py
+++ pipelines/reports/orquestracao_vitacare/schedules.py
@@ -6,5 +6,11 @@ schedules = [
     parameters={"environment": "prod"},
     interval="monthly",
     config={"day": 7, "hour": 16, "minute": 0},
-  )
+  ),
+  # FIXME: schedule de teste; remover
+  create_schedule(
+    parameters={"environment": "prod"},
+    interval="monthly",
+    config={"day": 29, "hour": 16, "minute": 0},
+  ),
 ]
