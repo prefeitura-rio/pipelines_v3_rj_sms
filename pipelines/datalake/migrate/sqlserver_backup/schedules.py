558633819eb126ccecca511b5a13da88a430d80c
Daniel Lira <danielllira@icloud.com>
2026-08-05 11:22:03 -0300

    chore lint

--- pipelines/datalake/migrate/sqlserver_backup/schedules.py
+++ pipelines/datalake/migrate/sqlserver_backup/schedules.py
@@ -4,6 +4,7 @@ from pipelines.utils.schedules import create_schedule_list
 flow_parameters = [
   {
     "backup_type": "vitacare_historic",
+    #"bucket_name": "vitacare_backups_gdrive", -- bucket utilizado anteriormente
     "bucket_name": "rj_subpav_vitacare_backups",
     "instance_name": "vitacare",
     "file_pattern": "vitacare_historic_*_*_*.bak",
@@ -11,6 +12,7 @@ flow_parameters = [
   },
   {
     "backup_type": "rnds_vaccine",
+    #"bucket_name": "vitacare_backups_gdrive", -- bucket utilizado anteriormente
     "bucket_name": "rj_subpav_vitacare_backups",
     "instance_name": "vitacare",
     "file_pattern": "RNDS_Vaccine_Historic_*.bak",
