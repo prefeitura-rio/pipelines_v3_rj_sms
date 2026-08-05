153bd4c4fa2aada957b6f93b3e9f34dcc2c6d3c1
Daniel Lira <danielllira@icloud.com>
2026-08-05 15:28:18 -0300

    Vitacare Backups - Origem Bucket (#94)
    * chore: muda bucket utilzado dos backups
    
    * chore: change resolved bucket name
    
    * chore lint
    
    * lint

--- pipelines/datalake/migrate/sqlserver_backup/schedules.py
+++ pipelines/datalake/migrate/sqlserver_backup/schedules.py
@@ -4,16 +4,18 @@ from pipelines.utils.schedules import create_schedule_list
 flow_parameters = [
   {
     "backup_type": "vitacare_historic",
-    "bucket_name": "vitacare_backups_gdrive",
+    # "bucket_name": "vitacare_backups_gdrive", -- bucket utilizado anteriormente
+    "bucket_name": "rj_subpav_vitacare_backups",
     "instance_name": "vitacare",
-    "file_pattern": "HISTÓRICO_PEPVITA_RJ/AP*/vitacare_historic_*_*_*.bak",
+    "file_pattern": "vitacare_historic_*_*_*.bak",
     "environment": "prod",
   },
   {
     "backup_type": "rnds_vaccine",
-    "bucket_name": "vitacare_backups_gdrive",
+    # "bucket_name": "vitacare_backups_gdrive", -- bucket utilizado anteriormente
+    "bucket_name": "rj_subpav_vitacare_backups",
     "instance_name": "vitacare",
-    "file_pattern": "HISTÓRICO_PEPVITA_RJ/RNDS/RNDS_Vaccine_Historic_*.bak",
+    "file_pattern": "RNDS_Vaccine_Historic_*.bak",
     "environment": "prod",
   },
 ]
