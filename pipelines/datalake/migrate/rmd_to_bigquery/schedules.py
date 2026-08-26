38ca44bade65619ad2dc0eb178dc70ea95bd7e68
Daniel Lira <danielllira@icloud.com>
2026-08-26 11:11:16 -0300

    style: ruff format

--- pipelines/datalake/migrate/rmd_to_bigquery/schedules.py
+++ pipelines/datalake/migrate/rmd_to_bigquery/schedules.py
@@ -2,16 +2,8 @@
 from pipelines.utils.schedules import create_schedule_list
 
 flow_parameters = [
-  {
-    "recurso": "RECURSO_FICHA_TUBERCULOSE",
-    "data_inicio": "D-1",
-    "environment": "prod",
-  },
-  {
-    "recurso": "RECURSO_FICHA_VIOLENCIA",
-    "data_inicio": "D-1",
-    "environment": "prod",
-  },
+  {"recurso": "RECURSO_FICHA_TUBERCULOSE", "data_inicio": "D-1", "environment": "prod"},
+  {"recurso": "RECURSO_FICHA_VIOLENCIA", "data_inicio": "D-1", "environment": "prod"},
   {
     "recurso": "RECURSO_EXAMES_LABORATORIAIS",
     "data_inicio": "D-1",
