1b6e3fdcce45a7e802e31b0bd8bfe0de0be2bbb5
Daniel Lira <danielllira@icloud.com>
2026-08-31 14:16:43 -0300

    fix: paramns

--- pipelines/reports/orquestracao_vitacare/constants.py
+++ pipelines/reports/orquestracao_vitacare/constants.py
@@ -18,9 +18,9 @@ class constants(Enum):
   # Parâmetros para sqlserver_backup (vitacare_historic)
   SQLSERVER_BACKUP_PARAMS = {
     "backup_type": "vitacare_historic",
-    "bucket_name": "vitacare_backups_gdrive",
+    "bucket_name": "rj_subpav_vitacare_backups",
     "instance_name": "vitacare",
-    "file_pattern": "HISTÓRICO_PEPVITA_RJ/AP*/vitacare_historic_*_*_*.bak",
+    "file_pattern": "vitacare_historic_*_*_*.bak",
     "environment": "prod",
   }
 
